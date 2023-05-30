3aa92c0e24986d6f073918cb487870d509dd7ba4
Shystra <jlsp.2019@hotmail.com>
2023-05-30 20:56:56 -0300

    configurin url imag in for aws

--- api/src/controllers/UsersController.ts
+++ api/src/controllers/UsersController.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { UsersServices } from "../services/UsersServices";
+import { s3 } from "../config/aws";
+import {v4 as uuid} from 'uuid';
+import { upload } from "../config/multer";
 
 class UsersController {
     private usersServices: UsersServices
@@ -46,12 +49,21 @@ class UsersController {
         //autenticacao
     }
 
-    update(request: Request, response: Response, next: NextFunction){
+    async update(request: Request, response: Response, next: NextFunction){
         const { name, oldPassword, newPassword } = request.body;
         console.log(request.file);
 
         try {
+            const avatar_url = request.file?.buffer;
+            const uploadS3 = await s3.upload({
+                Bucket: 'projeto-heroi',
+                Key: `${uuid()}-${request.file?.originalname}`,
+                // ACL: 'public-read',
+                Body: avatar_url,
+            }).promise();
             
+            console.log('url imagem =>', uploadS3.Location);
+
         } catch (error) {
             next(error);
         }
