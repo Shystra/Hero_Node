9271da3e68b84f2c981eeae0f689e3ad4f5f1361
Shystra <jlsp.2019@hotmail.com>
2023-05-31 23:01:48 -0300

    creating base dados in schedules

---
+++ api/src/services/SchedulesService.ts
@@ -0,0 +1,14 @@
+import { ICreate } from "../interfaces/Schedulesinterface";
+
+class SchedulesService{
+    create({ name, phone, date }: ICreate){
+
+
+
+        
+
+
+    }
+};
+
+export { SchedulesService }
