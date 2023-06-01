f37f67221ace43fef7880f5257c6e13d320ecbb5
Shystra <jlsp.2019@hotmail.com>
2023-05-31 23:22:44 -0300

    formating date

--- api/src/services/SchedulesService.ts
+++ api/src/services/SchedulesService.ts
@@ -1,16 +1,19 @@
 import { ICreate } from "../interfaces/Schedulesinterface";
-
+import { startOfHour } from 'date-fns';
 class SchedulesService{
     create({ name, phone, date }: ICreate){
         console.log("~ file: ScheduleService.ts:5 ~ SchedulesService ~ create ~ date:",
         date,
         );
 
+        const dateFormatted = new Date(date);
+        const hourStart = startOfHour(dateFormatted)
+        console.log(
+            "~ file: ScheduleService.ts:5 ~ SchedulesService ~ create ~ hourStart",
+            hourStart, 
+        );
 
-
-        return;
-
-
+    
     }
 };
 
