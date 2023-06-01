acfa227911f5af2cfef9d659f3ee8abc3ed2e0fb
Shystra <jlsp.2019@hotmail.com>
2023-05-31 23:15:16 -0300

    training date

--- api/src/services/SchedulesService.ts
+++ api/src/services/SchedulesService.ts
@@ -2,10 +2,13 @@ import { ICreate } from "../interfaces/Schedulesinterface";
 
 class SchedulesService{
     create({ name, phone, date }: ICreate){
+        console.log("~ file: ScheduleService.ts:5 ~ SchedulesService ~ create ~ date:",
+        date,
+        );
 
 
 
-        
+        return;
 
 
     }
