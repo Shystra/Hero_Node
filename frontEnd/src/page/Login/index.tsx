80146a7715ffc379cb2ba7420945cb74b250e8d8
Shystra <jlsp.2019@hotmail.com>
2023-06-02 21:55:02 -0300

    just css on login

--- frontEnd/src/page/Login/index.tsx
+++ frontEnd/src/page/Login/index.tsx
@@ -1,13 +1,27 @@
 import style from './Login.module.css';
+import logo from '../../assets/logo.webp'
 
 
 
 export function Login(){
     return (
         <div className={style.background}>
+            <div className={`container ${style.container}`}>
+                <div className={style.wrapper}>
 
-
-            
+                <div>
+                    <img src={logo} alt='' />
+                </div>
+                <div className={style.card}>
+                    <h2>Olá, seja bem vindo</h2>
+                    <form>
+                        <input type='text'/>
+                        <input type='text'/>
+                        <button>Entrar</button>
+                    </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
